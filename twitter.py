dabefbd2f115dd7994e1dc4892edfd57b8a3ba22
VladimirKogan <34720350+VladimirKogan@users.noreply.github.com>
2021-07-05 09:56:31 +0300

    Create twitter.py

---
+++ twitter.py
@@ -0,0 +1,78 @@
+import sys
+from selenium import webdriver
+import time
+from bot import main_func
+from tg import send_message_to_bot
+from datetime import datetime
+
+base_url=sys.argv[1]
+
+FIRST_TWEET_XML = "(//div[@lang='en'])[1]/span"
+SECOND_TWEET_XML = "(//div[@lang='en'])[2]/span"
+
+xPathPinnedText = '/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div/section/div/div/div[1]/div/div/article/div/div/div/div/div/div/div/div/div[2]/div/div/div/span'
+xPathRetweeted = '/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div/section/div/div/div[1]/div/div/article/div/div/div/div/div/div/div/div/div[2]/div/div/div/a/span'
+ELON_MUSK_TWITTER = "https://twitter.com/elonmusk"
+VLADIK = "https://twitter.com/Vladik01752005"
+
+
+def getTweetByNum(driver, num):
+    if if_tweet_retweet(driver, num) == 0:
+        xPathTweetsIfPinned = '/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div/section/div/div/div[' + str(num) + ']/div/div/article/div/div/div/div[2]/div[2]/div[2]/div/div/span'
+        return driver.find_element_by_xpath(xPathTweetsIfPinned).text
+    else:
+        return "retweeted"
+
+
+def if_has_pinned_tweet(driver):
+    try:
+        pinned = driver.find_element_by_xpath(xPathPinnedText)
+        return 3
+    except:
+        return 1
+
+
+
+def if_tweet_retweet(driver, num):
+    try:
+        retweet = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div/section/div/div/div["+str(num)+"]/div/div/article/div/div/div/div/div/div/div/div/div[2]/div/div/div/a/span")
+        return 1
+    except:
+        return 0
+
+def driverStart(name):
+    counter = 1
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('--disable-infobars')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--remote-debugging-port=9222')
+    driver = webdriver.Chrome(options=options, executable_path='/usr/bin/chromedriver')
+    driver.get(base_url)
+    LAST_TWEET = ''
+    while True:
+        while True:
+            try:
+                last_tweet = getTweetByNum(driver, 1)
+                break
+            except:
+                time.sleep(0.01)
+        current_time = datetime.now().strftime("%H:%M:%S")
+        first_tweet_num = if_has_pinned_tweet(driver)
+        last_tweet = getTweetByNum(driver, first_tweet_num)
+        print(f"Current Time ({counter})=, {str(current_time)}: {last_tweet}")
+        counter += 1
+        if LAST_TWEET == '':
+            LAST_TWEET = last_tweet
+        elif LAST_TWEET != last_tweet and last_tweet != 'retweeted':
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            print("Current Time =", current_time)
+            main_func(last_tweet)
+            send_message_to_bot("TWITTERPARSE: " + last_tweet)
+            new_tweet = last_tweet
+            LAST_TWEET = new_tweet
+
+        driver.refresh()
+driverStart(base_url)
